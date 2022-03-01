63ea5385a2d9ac4c560ceb9837318522229d3466
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-03-01 14:23:40 +0530

    hiding and showing menu on toggle button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,45 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 
+const navMenu = document.querySelector(".nav__menu"),
+  navToggle = document.querySelector(".nav__toggle"),
+  navClose = document.querySelector(".nav__close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
-
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
-
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 /*==================== REMOVE MENU MOBILE ====================*/
+// when we click navigation link it removes menu from screen
+const navLinks = document.querySelectorAll(".nav__link");
 
-
+const linkAction = () => {
+  navMenu.classList.remove("show-menu");
+};
+navLinks.forEach((n) => n.addEventListener("click", linkAction));
 /*==================== ACCORDION SKILLS ====================*/
-
 
 /*==================== QUALIFICATION TABS ====================*/
 
-
 /*==================== SERVICES MODAL ====================*/
-
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
-
 /*==================== TESTIMONIAL ====================*/
-
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
