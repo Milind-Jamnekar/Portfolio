be8317a7a276b29d2ab33c54edd219215fc19058
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-03-04 22:08:51 +0530

    qualification section is done

---
+++ assets/js/animation.js
@@ -0,0 +1,13 @@
+gsap.from([".home__data h1", ".home__data h3"], 1.8, {
+  y: 100,
+  ease: "power4.out",
+  opacity: 0,
+  delay: 1,
+  skewY: 10,
+  stagger: {
+    amount: 0.5,
+  },
+});
+// .from(".home__data h3", 1.8, {
+//     y:
+// })
